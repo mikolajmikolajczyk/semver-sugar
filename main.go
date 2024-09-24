fbf4aa5ad5729fefd0930ef3581ce7e64931bc24
Mikołaj Mikołajczyk <mikolaj.mikolajczyk@cumulocity.com>
2024-09-24 15:45:22 +0200

    Business logic added

--- main.go
+++ main.go
@@ -3,24 +3,13 @@ package main
 import (
 	"fmt"
 	"os"
-	"strconv"
-	"time"
 
 	"github.com/actions-go/toolkit/core"
 )
 
 func runMain() {
-	sleep := os.Getenv("INPUT_MILLISECONDS")
-	core.Debug(fmt.Sprintf("Waiting %s milliseconds", sleep))
-	core.Debug(time.Now().String())
-	delay, err := strconv.Atoi(sleep)
-	if err != nil {
-		core.Error(err.Error())
-		return
-	}
-	time.Sleep(time.Duration(delay) * time.Millisecond)
-	core.Debug(time.Now().String())
-	core.SetOutput("time", time.Now().String())
+	release_branch := os.Getenv("INPUT_RELEASE_BRANCH")
+	core.Debug(fmt.Sprintf("Release branch is %s", release_branch))
 }
 
 func main() {
